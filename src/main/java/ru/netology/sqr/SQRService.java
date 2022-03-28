20ffcefaf9269d896b36a8dc2b0a6dae9e15ccba
Jhamyl Tukhvatullin <sinkler86@gmail.com>
2022-03-28 17:56:43 +0700

    initial commit

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,9 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    int minRange = 200;
+    int maxRange = 300;
+
+    SQRService service = new SQRService();
+    system.out.println ("Количество квдратов в диапазоне: " + service.shouldNumberOfSQR(minRange,maxRange));
+}
