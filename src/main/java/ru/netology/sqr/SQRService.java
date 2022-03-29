88e28a1f02f9cae63a5f3ddfa86d0f6fc13c792f
Jamyl Tukhvatullin <89373468+Sinkler86@users.noreply.github.com>
2022-03-29 19:36:32 +0700

    Delete src directory

--- src/main/java/ru/netology/sqr/SQRService.java
+++
@@ -1,9 +0,0 @@
-package ru.netology.sqr;
-
-public class SQRService {
-    int minRange = 200;
-    int maxRange = 300;
-
-    SQRService service = new SQRService();
-    system.out.println ("Количество квдратов в диапазоне: " + service.shouldNumberOfSQR(minRange,maxRange));
-}
